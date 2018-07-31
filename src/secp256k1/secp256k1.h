4dec9b6ee6049b632fc79937ebf70f23ecd4b4bc
realChainLife <realchainlife@protonmail.com>
2018-07-31 20:15:34 +0300

    add copyright descriptions

--- src/secp256k1/secp256k1.h
+++ src/secp256k1/secp256k1.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2009-2014 The Bitcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #ifndef _SECP256K1_
 # define _SECP256K1_
 
