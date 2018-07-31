ea0a5f3e1742dc836542955a38b604d0970a2d62
realChainLife <realchainlife@protonmail.com>
2018-07-31 18:11:25 +0300

    change client versioning

--- src/clientversion.h
+++ src/clientversion.h
@@ -23,10 +23,10 @@
 #define CLIENT_VERSION_IS_RELEASE true
 
 /**
- * Copyright year (2018-this)
+ * Copyright year (2009-this)
  * Todo: update this when changing our copyright comments in the source
  */
-#define COPYRIGHT_YEAR 2017
+#define COPYRIGHT_YEAR 2018
 
 #endif //HAVE_CONFIG_H
 
@@ -38,7 +38,7 @@
 #define DO_STRINGIZE(X) #X
 
 //! Copyright string used in Windows .rc files
-#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Core Developers, 2014-" STRINGIZE(COPYRIGHT_YEAR) " The Dash Core Developers, 2015-" STRINGIZE(COPYRIGHT_YEAR) " The LightPayCoin Core Developers"
+#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Core Developers, 2014-" STRINGIZE(COPYRIGHT_YEAR) " The Dash Core Developers, 2018-" STRINGIZE(COPYRIGHT_YEAR) " The BaysicCoin Core Developers"
 
 /**
  * lightpaycoind-res.rc includes this file, but it cannot cope with real c++ code.
