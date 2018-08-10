97c50564740dd5e81a93890c0aabc661eed133b4
realChainLife <realchainlife@protonmail.com>
2018-08-10 22:29:17 +0300

    edit copyright listing

--- src/obfuscation-relay.cpp
+++ src/obfuscation-relay.cpp
@@ -1,3 +1,9 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2014 The Bitcoin developers
+// Copyright (c) 2018 The LightPayCoin developers
+// Copyright (c) 2018 The Baysic developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "obfuscation-relay.h"
 
