72192b9b8824689fca98a5641c1a5a9f8af7f131
RealChain Life <realchainlife@protonmail.com>
2018-10-17 16:16:26 +0300

    modifications, additions and deletions

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -219,9 +219,9 @@ public:
         vFixedSeeds.clear();
         vSeeds.clear();
 /*
-        vSeeds.push_back(CDNSSeedData("80.211.128.125", "80.211.128.125"));         // Single node address
-        vSeeds.push_back(CDNSSeedData("94.177.187.54", "94.177.187.54"));           // Single node address
-        vSeeds.push_back(CDNSSeedData("80.211.129.187", "80.211.129.187"));         // Single node address
+        vSeeds.push_back(CDNSSeedData("45.76.111.81", "45.76.111.81"));           // Single node address 
+        vSeeds.push_back(CDNSSeedData("108.61.166.37", "108.61.166.37"));         // Single node address 
+        vSeeds.push_back(CDNSSeedData("95.179.170.82", "95.179.170.82"));         // Single node address
 */
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 85); // Testnet baisyccoin addresses start with 'x' or 'y'
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 19);  // Testnet baisyccoin script addresses start with '8' or '9'
